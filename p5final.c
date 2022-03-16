bf638ba4876e9bb8d994ef11faaec9b1d91817a3
GodhaRG <96509418+GodhaRG@users.noreply.github.com>
2022-03-16 04:29:59 +0000

    done 5

--- p5final.c
+++ p5final.c
@@ -1,50 +1,55 @@
 #include <stdio.h>
 #include<math.h>
-int input_array_size()
+int input()
 {
-  int a;
-  printf("enter the size of the array\n");
-  scanf("%d",&a);
-  return a;
+  int n;
+  printf("Enter the size of the array\n");
+  scanf("%d",&n);
+  return n;
 }
-void init_array(int n,int a[n])
+
+void init_array(int n, int a[n])
 {
   for(int i=0;i<n;i++)
-     
-      a[i]=i;
-      a[0]=0;
-      a[1]=0;
-      
-}
-void erotosthenes_sieve(int n,int a[n])
-{
-  int i=0;
-  while(i<sqrt(n))
     {
-      while(a[i]==0)
-        i++;
-      for(int k=i+i;i<n;k+=i)
-        {
-        a[k]=0;
-      i++;
-          }
-    }
+    a[i] = i+1;
+      }
 }
-void out_put(int n,int a[n])
+
+void ets(int n, int a[n])
 {
+  int i,j;
+for(int i=1;i<n;i++)
+    {
+    if(a[i]==0)
+      continue;
+      for(j=a[i]+1;j<=n;j++)
+        {
+          if(j%a[i]==0)
+            a[j-1]=0;
+        }
+  }
+}
+
+
+void display(int n, int a[n])
+{ 
+  printf("the prime numbers are\n");
   for(int i=0;i<n;i++)
     {
       if(a[i]!=0)
-        printf("%d" ,a[i]);
-    }
-  printf("\n");
+      {
+    printf(" %d  ",a[i]);
 }
+      }
+  }
 int main()
 {
-  int n,a[n];
-  n=input_array_size();
+  int n;
+  n=input();
+  int a[n];
   init_array(n,a);
-  erotosthenes_sieve(n,a);
-  out_put(n,a);
+  ets(n,a);
+  display(n,a);
   return 0;
 }
